5a9ade78481987f999d77e1fca10a8e5f1f7be92
Vladislav Boiko <boikovladislav@gmail.com>
2020-08-21 16:09:05 +0200

    feat: building as a browser plugin

---
+++ dist/precache-manifest.e47e414d05b2a87a6b5022eb9ac59fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20392b093b2f22a966b9bc4eae9647c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364aa159354a180e6ae2",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "6fc9a27c06506497817a",
+    "url": "/static/js/2.f2e7455d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2e7455d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364aa159354a180e6ae2",
+    "url": "/static/js/main.57a8b270.chunk.js"
+  },
+  {
+    "revision": "649496b1742c5530d9c8",
+    "url": "/static/js/runtime-main.d47b4ec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
