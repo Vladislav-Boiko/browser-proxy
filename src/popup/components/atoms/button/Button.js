644e9867ef6545c280035d03b65859782797f120
Vladislav-Boiko <boikovladislav@gmail.com>
2020-12-05 10:12:32 +0100

    feat: adding tretiary button and a iconed buttons

--- src/popup/components/atoms/button/Button.js
+++ src/popup/components/atoms/button/Button.js
@@ -2,18 +2,28 @@ import React from 'react';
 import cn from 'classnames';
 
 import './Button.css';
-const Button = ({ children, onClick, className, primary, secondary }) => {
+const Button = ({
+  children,
+  onClick,
+  className,
+  Icon,
+  primary,
+  secondary,
+  tretiary,
+}) => {
   return (
     <button
       className={cn('button label_medium py1 px3', className, {
         'primary white-color primary-bg': primary,
         'secondary white-bg primary-color': secondary,
+        'tretiary white-bg primary-color': tretiary,
       })}
       onClick={(e) => {
         e.preventDefault();
-        onClick();
+        onClick && onClick();
       }}
     >
+      {Icon && <Icon className={'icon_md mr1'} />}
       {children}
     </button>
   );
--- src/popup/components/atoms/button/Button.stories.js
+++ src/popup/components/atoms/button/Button.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from './Button';
+import Icons from '../Icons/Icons';
 
 export default {
   title: 'Button',
@@ -8,14 +9,32 @@ export default {
 };
 
 const Template = (args) => (
-  <div>
-    <Button {...args} primary>
-      Primary
-    </Button>
-    <Button {...args} secondary className="ml6">
-      Secondary
-    </Button>
-  </div>
+  <React.Fragment>
+    <h3>Regular buttons</h3>
+    <div className="my4">
+      <Button {...args} primary>
+        Primary
+      </Button>
+      <Button {...args} secondary className="mt6">
+        Secondary
+      </Button>
+      <Button {...args} tretiary className="mt6">
+        Tretiary
+      </Button>
+    </div>
+    <h3>Buttons with icons</h3>
+    <div className="my4">
+      <Button {...args} primary Icon={Icons.Enable}>
+        Primary
+      </Button>
+      <Button {...args} secondary className="mt6" Icon={Icons.AddFolder}>
+        Secondary
+      </Button>
+      <Button {...args} tretiary className="mt6" Icon={Icons.Add}>
+        Tretiary
+      </Button>
+    </div>
+  </React.Fragment>
 );
 
 export const Story = Template.bind({});
