0df3a8e9be3d02c64e0934e7453b1113b19c6bf7
Vladislav-Boiko <boikovladislav@gmail.com>
2021-01-28 19:59:28 +0100

    chore: remove unnecessary import

--- src/injected/xhr/TrackXhr.js
+++ src/injected/xhr/TrackXhr.js
@@ -2,7 +2,6 @@ import messaging from '../../common/communication/injected/ProxyMessaging';
 import { v4 as uuid } from 'uuid';
 import EVENTS from '../../common/communication/injected/events';
 import { DOMAIN } from '../../content/constants';
-import { parseXhrHeaders } from './utils';
 
 export const trackXhr = (requestPayload, xhr) => {
   const id = uuid();
